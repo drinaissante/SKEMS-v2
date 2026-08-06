a602df8090a8922b29b1bcb6c77d4953a5d5b927
Aldrin Victoria <victoriaaldrin812@gmail.com>
2026-08-06 22:46:28 +0800

    feat: added soc meds, reworked footer

--- skems-frontend/src/components/Footer.tsx
+++ skems-frontend/src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { FaFacebook, FaTiktok } from "react-icons/fa6"
 import { Link } from "react-router-dom";
 
 export default function Footer() {
@@ -21,27 +22,28 @@ export default function Footer() {
   }, [])
 
   return (
-    <footer className="bg-[#050505] mt-auto text-base/5 pb-7">
+    <footer className="bg-[#050505] text-base/4.8 pb-3">
 
+      {/* the gold bar */}
       <div
         ref={lineRef}
         className={`h-0.5 mx-auto mb-8 bg-linear-to-r from-transparent via-[#fdb125] to-transparent ${shown ? "animate-gold-grow" : ""}`}
       />
 
-      <div className="max-w-7xl mx-auto flex flex-col gap-10 items-start pl-7 md:pl-4 md:gap-1 sm:pl-0 sm:flex-row sm:items-center justify-between">
+      <div className="max-w-7xl mx-auto flex flex-col items-start pl-5 pr-5 justify-between gap-8 sm:flex-row sm:gap-12">
         
         <div className="flex flex-col gap-2 select-none">
-          <div className="text-lg font-bold text-[#fdb125] uppercase">
+          <div className="text-xs font-bold text-[#fdb125] uppercase">
             Sine Kultura
           </div>
 
-          <div className="text-gray-400/50 max-w-sm">
+          <div className="text-sm text-gray-400/50 max-w-xs">
             Capturing the culture and arts through the lens of passionate student filmmakers and photographers at Bulacan State University.
           </div>
         </div>
 
-        <div className="flex flex-col gap-3 select-none [&_span]:transition-colors [&_span]:duration-200 [&_span]:hover:text-[#c89116]">
-          <div className="text-lg font-bold text-white uppercase">
+        <div className="flex flex-col select-none [&_span]:transition-colors [&_span]:duration-200 [&_span]:hover:text-[#c89116] [&_span]:text-xs">
+          <div className="text-xs font-bold text-white uppercase">
             Explore
           </div>
 
@@ -60,19 +62,40 @@ export default function Footer() {
 
         </div>
 
-        <div className="flex flex-col gap-3">
-          <div className="text-lg font-bold text-white uppercase">
+        <div className="flex flex-col gap-3 [&_span]:text-sm">
+          <div className="text-sm font-bold text-white uppercase">
             Contact Us
           </div>
 
-          <span>Email: official@sinekultura.com</span>
+          <span>Email: bulsusinekultura@gmail.com</span>
           <span>Location: BulSU Office of Culture and Arts</span>
           
         </div>
+        
+      
+        <div className="flex flex-col gap-3">
+            <div className="text-sm font-bold text-white uppercase">
+              Follow Us
+            </div>
+            
+            <div className="flex gap-2 cursor-pointer hover:text-[#c89116] transition-colors duration-200">
+              <a href="https://www.facebook.com/sinekulturabulsu" target="/">
+                <FaFacebook size={24} /> 
+              </a>
+              <span className="text-xs italic">@sinekulturabulsu</span>
+            </div>
+
+            <div className="flex gap-2 cursor-pointer hover:text-[#c89116] transition-colors duration-200">
+              <a href="https://www.tiktok.com/@bulsu.sinekultura" target="/">
+                <FaTiktok size={24} /> 
+              </a>
+              <span className="text-xs italic">@bulsu.sinekultura</span>
+            </div>
+        </div>
+
           
       </div>
-      
-      <p className="text-center align-middle pt-10 text-[#a6a6a6]/50 sm:text-xs">
+      <p className="text-center align-middle pt-5 text-[#a6a6a6]/50 text-xs">
         &copy; {new Date().getFullYear()} Sine Kultura All rights reserved.
       </p>
 
