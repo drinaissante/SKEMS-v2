afc05b6b2802d2ab25ca3478b71fb9aa502cffb1
Aldrin Victoria <victoriaaldrin812@gmail.com>
2026-08-08 23:02:29 +0800

    feat: now scrolls when switching pages

--- skems-frontend/src/main.tsx
+++ skems-frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
+import ScrollToTop from './components/ScrollToTop.tsx'
 
 import './index.css'
 
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <ScrollToTop />
         <AuthProvider>
           <App />
         </AuthProvider>
