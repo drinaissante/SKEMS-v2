10fbbe5e0bc1e071f53919ff9a488a4d850eab85
Aldrin Victoria <victoriaaldrin812@gmail.com>
2026-08-19 20:19:05 +0800

    style: made card smaller

--- skems-frontend/src/pages/admin/RequestsPage.tsx
+++ skems-frontend/src/pages/admin/RequestsPage.tsx
@@ -205,7 +205,7 @@ export default function RequestsPage() {
           </p>
         ) : (
           <>
-            <div className="space-y-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
               {paginatedItems.map((r) => (
                 <div
                   key={r.id}
